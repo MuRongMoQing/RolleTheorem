1ac26ae3117d317b030e6b730f389aa242f496dd
An Ziwu <14214576@cumt.edu.cn>
2026-07-07 22:23:39 +0800

    添加基本函数和类型检查

---
+++ RolleTheorem/functions_test.lean
@@ -0,0 +1,9 @@
+def f (n : Nat) : String := toString n
+def g (s : String) : Bool := s.length > 0
+
+#check fun x : Nat => x
+#check fun x : Nat => true
+#check fun x : Nat => g (f x)
+#check fun x => g (f x)
+
+#check fun (g : String → Bool) (f : Nat → String) (x : Nat) => g (f x)
