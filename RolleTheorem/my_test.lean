8df4ecc47f63da12e0cf7c02dba588838893e412
An Ziwu <14214576@cumt.edu.cn>
2026-07-07 22:19:53 +0800

    ++++

--- RolleTheorem/my_test.lean
+++ RolleTheorem/my_test.lean
@@ -25,3 +25,7 @@ def α : Bool := true
 #check λ (x : Nat) => x + 5
 
 #eval (λ x : Nat => x + 5) 10
+
+#check fun x : Nat => fun y : Bool => if not y then x + 1 else x + 2
+#check fun (x : Nat) (y : Bool) => if not y then x + 1 else x + 2
+#check fun x y => if not y then x + 1 else x + 2
