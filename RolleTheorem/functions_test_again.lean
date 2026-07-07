d4ea1cd75af5d802f58185257329adb31f60aca5
An Ziwu <14214576@cumt.edu.cn>
2026-07-07 22:27:10 +0800

    添加额外的评估示例以测试函数

--- RolleTheorem/functions_test_again.lean
+++ RolleTheorem/functions_test_again.lean
@@ -5,3 +5,5 @@ def g (s : String) : Bool := s.length > 0
 
 #check
   (fun (α β γ : Type) (u : β → γ) (v : α → β) (x : α) => u (v x)) Nat String Bool g f 0
+  #eval (fun x : Nat => x) 1
+  #eval (fun x : Nat => true) 1
