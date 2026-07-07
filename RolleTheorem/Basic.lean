d8e413909db46acafa8bb1e3bf641808ed96bc72
An Ziwu <14214576@cumt.edu.cn>
2026-07-07 15:33:31 +0800

    basic

--- RolleTheorem/Basic.lean
+++ RolleTheorem/Basic.lean
@@ -1,1 +1,24 @@
-def hello := "world"
+/- 定义一些常数 -/
+
+def m : Nat := 1       -- m 是自然数
+def n : Nat := 0
+def b1 : Bool := true  -- b1 是布尔型
+def b2 : Bool := false
+
+/- 检查类型 -/
+
+#check m
+#check n
+#check n + 0
+#check m * (n + 0)
+#check b1
+-- "&&" 是布尔与
+#check b1 && b2
+-- 布尔或
+#check b1 || b2
+-- 布尔 "真"
+#check true
+/- 求值（Evaluate） -/
+#eval 5 * 4
+#eval m + 2
+#eval b1 && b2
