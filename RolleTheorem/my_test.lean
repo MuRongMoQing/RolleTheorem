3e92fb86f8642167fc06ac66973ff24450c86f4d
An Ziwu <14214576@cumt.edu.cn>
2026-07-07 21:46:34 +0800

    again+

--- RolleTheorem/my_test.lean
+++ RolleTheorem/my_test.lean
@@ -4,3 +4,17 @@ def b : Type := a
 #check b
 def x : a := Nat.succ 0
 #check x
+def y : Nat := Nat.succ 0
+#check List a
+#check List Nat
+example : List Nat := [1, 2, 3]
+def z : List a := [Nat.succ 0, Nat.succ 1]
+#check z
+
+universe u
+
+def F (α : Type u) : Type u := Prod α α
+
+#check F
+#check F Nat
+#check F (List Nat)
