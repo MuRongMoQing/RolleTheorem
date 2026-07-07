9b734f5ffadd322ee90fe0bc4d9069b37a8fa34c
An Ziwu <14214576@cumt.edu.cn>
2026-07-07 15:49:08 +0800

    try again

---
+++ RolleTheorem/build_def_last.lean
@@ -0,0 +1,12 @@
+#check Nat.succ
+#check (0, 1)
+#check Nat.add
+#check Nat.succ 2
+#check Nat.add 3
+#check Nat.add 5 2
+#check (5, 9).1
+#check (5, 9).2
+#eval Nat.succ 2
+#eval Nat.add 5 2
+#eval (5, 9).1
+#eval (5, 9).2
