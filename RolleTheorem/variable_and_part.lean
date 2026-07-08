265e05ecfa3445b63746044dcbe54f62a003db19
An Ziwu <14214576@cumt.edu.cn>
2026-07-08 23:05:35 +0800

    重构 compose、doTwice 和 doThrice 函数，移除多余的类型参数

--- RolleTheorem/variable_and_part.lean
+++ RolleTheorem/variable_and_part.lean
@@ -1,11 +1,14 @@
-def compose (α β γ : Type) (g : β → γ) (f : α → β) (x : α) : γ :=
+variable (α β γ : Type)
+
+def compose (g : β → γ) (f : α → β) (x : α) : γ :=
   g (f x)
 
-def doTwice (α : Type) (h : α → α) (x : α) : α :=
+def doTwice (h : α → α) (x : α) : α :=
   h (h x)
 
-def doThrice (α : Type) (h : α → α) (x : α) : α :=
+def doThrice (h : α → α) (x : α) : α :=
   h (h (h x))
+
 
 #eval doTwice Nat (fun x => x + 3) 5
 #eval doThrice Nat (fun x => x + 1) 0
