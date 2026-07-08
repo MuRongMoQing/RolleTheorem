73d5f56494817d3c995822bab34748dc5c5f4797
An Ziwu <14214576@cumt.edu.cn>
2026-07-08 21:41:41 +0800

    添加多个函数及其评估示例以扩展功能

--- RolleTheorem/part_definiions.lean
+++ RolleTheorem/part_definiions.lean
@@ -4,3 +4,15 @@ def twice_double (x : Nat) : Nat :=
   let y := x + x; y * y
 
 #eval twice_double 2
+
+def foo := let a := Nat; fun x : a => x + 2
+#eval foo 1
+
+def foo_another := fun x : Nat => x + 2
+#eval foo_another 1
+
+def foo_other_another(a : Type) (x : a) : a := x
+#eval foo_other_another Nat 1
+
+def foo_the_another := fun(a : Type) => fun(x : a) => x
+#eval foo_the_another Nat 1
