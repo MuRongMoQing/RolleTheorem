10bde42f61c337b5e87af21bc7d80fa4ecb8096d
An Ziwu <14214576@cumt.edu.cn>
2026-07-08 23:03:40 +0800

    更新 doTwice 函数的评估示例，使用加法 3 和输入 5

--- RolleTheorem/variable_and_part.lean
+++ RolleTheorem/variable_and_part.lean
@@ -7,6 +7,6 @@ def doTwice (α : Type) (h : α → α) (x : α) : α :=
 def doThrice (α : Type) (h : α → α) (x : α) : α :=
   h (h (h x))
 
-#eval doTwice Nat (fun x => x + 1) 0
+#eval doTwice Nat (fun x => x + 3) 5
 #eval doThrice Nat (fun x => x + 1) 0
 #eval compose Nat Nat Nat (fun x => x + 1) (fun x => x * 2) 3
